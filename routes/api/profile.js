79487c722f45943dd4c29c861908acb8a91c7d74
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-08-25 21:04:49 +0500

    Created Routein Profile for accessing a profile for current user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
+const Profile = require("../../modals/Profile");
+const User = require("../../modals/User");
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
 
-router.get("/", (req, res) => res.send("Profile Route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    ); // The reason for setting the user user.id is that in Profile schema user is an id not a name
+    // The populate method helps to get things from user modal instead of profile
+    if (!profile) {
+      return res.status(400).json({ msg: "there is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+}); // Now its a protected route as we have added auth here as 2nd arg
 
 module.exports = router;
