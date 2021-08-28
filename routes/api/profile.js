bef4657a771fc1accafc040a4e5ee9b2eb8fef44
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-08-29 00:06:14 +0500

    added route for deleing the experience of the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -213,7 +213,7 @@ router.put(
   }
 );
 
-// @route   PUT api/profile/experience/:experience_id
+// @route   POST api/profile/experience/:experience_id
 // @desc    Update Profile experience
 // @access  Private
 
@@ -271,4 +271,23 @@ router.post(
   }
 );
 
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const foundProfile = await Profile.findOne({ user: req.user.id });
+
+    foundProfile.experience = foundProfile.experience.filter(
+      (exp) => exp._id.toString() !== req.params.exp_id
+    );
+
+    await foundProfile.save();
+    return res.status(200).json(foundProfile);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Server error" });
+  }
+});
 module.exports = router;
