61b24b6b9d7e522e777dcd87048772d81cb90ffe
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-08-30 18:45:17 +0500

    Added post model in Modals folder

---
+++ modals/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user", // this connects user schema to post schema so that only user can create post for itself not for others
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId, // so that only one like can be done by a user and also we can know which user the like came from
+        refs: "user",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        refs: "user",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
@@ -217,8 +219,65 @@ router.put(
 // @desc    Update Profile experience
 // @access  Private
 
-router.post(
-  "/experience/:experience_id",
+// router.post(
+//   "/experience/:experience_id",
+//   [
+//     auth,
+//     [
+//       check("title", "Title is Required").not().isEmpty(),
+//       check("company", "Company is Required").not().isEmpty(),
+//       check("from", "From Date is Required").not().isEmpty(),
+//     ],
+//   ],
+//   async (req, res) => {
+//     const experience_id = req.params.experience_id;
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+//     try {
+//       let profile = await Profile.findOne({
+//         user: req.user.id,
+//       });
+//       if (experience_id) {
+//         profile = await Profile.findOneAndUpdate(
+//           {
+//             $and: [
+//               { user: req.user.id },
+//               { experience: { $elemMatch: { _id: req.params.experience_id } } },
+//             ],
+//           },
+//           {
+//             $set: {
+//               "experience.$.current": req.body.current,
+//               "experience.$.title": req.body.title,
+//               "experience.$.company": req.body.company,
+//               "experience.$.location": req.body.location,
+//               "experience.$.from": req.body.from,
+//               "experience.$.to": req.body.to,
+//               "experience.$.description": req.body.description,
+//             },
+//           },
+//           { new: true } // used to return updated object
+//         );
+//         return res.json(profile);
+//       }
+//       return res.status(400).json({ msg: "Profile Experience Not Found" });
+//     } catch (error) {
+//       console.error(error.message);
+//       if (error.kind == "ObjectId") {
+//         return res.status(400).json({ msg: "Profile Not Found" });
+//       }
+//       res.status(500).send("Server Error");
+//     }
+//   }
+// );
+
+// @route   PUT api/profile/experience/:exp_id
+// @desc    Update Profile experience
+// @access  Private
+router.put(
+  "/experience/:exp_id",
   [
     auth,
     [
@@ -228,44 +287,35 @@ router.post(
     ],
   ],
   async (req, res) => {
-    const experience_id = req.params.experience_id;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+    const exp = { title, company, from };
+    if (location) exp.location = location;
+    if (to) exp.to = to;
+    if (current) exp.current = current;
+    if (description) exp.description = description;
+
     try {
-      let profile = await Profile.findOne({
-        user: req.user.id,
-      });
-      if (experience_id) {
-        profile = await Profile.findOneAndUpdate(
-          {
-            $and: [
-              { user: req.user.id },
-              { experience: { $elemMatch: { _id: req.params.experience_id } } },
-            ],
+      const profile = await Profile.findOneAndUpdate(
+        { user: req.user.id, "experience._id": req.params.exp_id },
+        {
+          $set: {
+            // preserving the experience id
+            "experience.$": { _id: req.params.exp_id, ...exp },
           },
-          {
-            $set: {
-              "experience.$.current": req.body.current,
-              "experience.$.title": req.body.title,
-              "experience.$.company": req.body.company,
-              "experience.$.location": req.body.location,
-              "experience.$.from": req.body.from,
-              "experience.$.to": req.body.to,
-              "experience.$.description": req.body.description,
-            },
-          },
-          { new: true } // used to return updated object
-        );
-        return res.json(profile);
-      }
-      return res.status(400).json({ msg: "Profile Experience Not Found" });
+        },
+        {
+          new: true,
+        }
+      );
+      res.json(profile);
     } catch (error) {
-      console.error(error.message);
-      if (error.kind == "ObjectId") {
-        return res.status(400).json({ msg: "Profile Not Found" });
-      }
+      console.log(error.message);
       res.status(500).send("Server Error");
     }
   }
@@ -290,4 +340,102 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     return res.status(500).json({ msg: "Server error" });
   }
 });
+
+// @route   PUT api/profile/education
+// @desc    Add Profile education
+// @access  Private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is Required").not().isEmpty(),
+      check("degree", "Degree is Required").not().isEmpty(),
+      check("fieldofstudy", "Field Of Study Date is Required").not().isEmpty(),
+      check("from", "From Date is Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructuring Data
+    const { school, degree, fieldofstudy, from, to, current, description } =
+      req.body;
+    // assigning destructured values to new variable
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      // Adding Experience not updating
+      // pushing or inserting into the beginning experience array
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route    DELETE api/profile/education/:exp_id
+// @desc     Delete education from profile
+// @access   Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const foundProfile = await Profile.findOne({ user: req.user.id });
+
+    foundProfile.education = foundProfile.education.filter(
+      (edu) => edu._id.toString() !== req.params.edu_id
+    );
+
+    await foundProfile.save();
+    return res.status(200).json(foundProfile);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Server error" });
+  }
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos for github
+// @access   Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        res.sendStatus(404).json({ msg: "No Github profile found" });
+      }
+      return res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Server error" });
+  }
+});
+
 module.exports = router;
